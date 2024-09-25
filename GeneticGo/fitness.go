7dda8a10b2ac540fc7a334652dd488b362092c25
hoisin-sauce <97987829+hoisin-sauce@users.noreply.github.com>
2024-09-25 11:35:00 +0100

    Changed how overlap was calculated

--- GeneticGo/fitness.go
+++ GeneticGo/fitness.go
@@ -17,24 +17,36 @@ func GetBoundsPenalty(c chromosome, populationSize int)(penalty int){
 
 
 func GetOverlapPortions(chromosome1 chromosome, chromosome2 chromosome)(overlapPortions uint32){
-	chromosomeOverlap := chromosome1.gene & chromosome2.gene
+	// bitwise & to find overlapping sections
+	// chromosomeOverlap := chromosome1.gene & chromosome2.gene
 
+	// go through all masks to check for identical
 	for category, mask := range masks{
-		if chromosomeOverlap & mask == chromosome1.gene & mask{
+		// apply masks and check if region is identical
+		if chromosome1.gene & mask == chromosome2.gene & mask{
+			// if conditions are met note it in overlap
 			overlapPortions |= checkMap[category]
 		}
 	}
+
 	return
+
 }
 
 func CheckOverlapPortions(overlapPortions uint32)(checksFailed int, checkCount int){
+
+	// got through all checks
 	for j := 0; j < len(comparisonChecks); j++ {
 		checkCount++
+
+		// isolate areas in check and check if equal to the check
 		if (comparisonChecks[j] & overlapPortions) == comparisonChecks[j]{
 			checksFailed++
 		}
 	}
+
 	return
+
 }
 
 func GetOverlap(c chromosome, population []chromosome)(overlapCount int, totalCount int){
@@ -44,14 +56,20 @@ func GetOverlap(c chromosome, population []chromosome)(overlapCount int, totalCo
 		checkCount        int
 	)
 
+	// for each member of the population
 	for i := 0; i < len(population); i++ {
+		// get the overlap between the member and the current chromosome
 		overlapPortions = GetOverlapPortions(c, population[i])
+
+		// check if the overlap is in any crucial regions
 		checksFailed, checkCount = CheckOverlapPortions(overlapPortions)
+
 		overlapCount += checksFailed
 		totalCount += checkCount
 	}
 
 	return
+
 }
 
 func GetFitness(c chromosome, population []chromosome)(fitness float64){
@@ -59,11 +77,16 @@ func GetFitness(c chromosome, population []chromosome)(fitness float64){
 		overlapCount      int
 		totalChecks       int
 		overlap           float64
-		total            float64
+		total             float64
 	)
 
+	// get the overlap
 	overlapCount, totalChecks = GetOverlap(c, population)
+
+	// apply bounds penalty
 	overlapCount += GetBoundsPenalty(c, len(population))
+
+	// calculate GetFitness
 	overlap, total = float64(overlapCount), float64(totalChecks)
 	fitness = (total - overlap)/ total
 	c.fitness = fitness
